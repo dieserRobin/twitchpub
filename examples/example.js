047861a13ecf2b4b6fdd70750969bbc3ae61539f
Robin Klein <robin@klein.im>
2020-08-19 13:51:32 +0200

    init

---
+++ examples/example.js
@@ -0,0 +1,22 @@
+const TwitchPubSub = require("./index");
+
+const PubSub = new TwitchPubSub({
+  oauth: "OAUTH TOKEN",
+  subscriptions: [
+    {
+      topic:
+        "TOPIC (channel-bits,channel-points,channel-subscriptions,channel-bits-badge,chat-moderatoractions,whispers)",
+      channel: "CHANNEL ID",
+    },
+  ],
+});
+
+PubSub.on("connected", () => {
+  console.log("PubSub connected");
+});
+
+PubSub.on("error", (error) => console.log(error));
+
+PubSub.on("message", (msg) => {
+  console.log(msg);
+});
